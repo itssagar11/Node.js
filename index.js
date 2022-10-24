9b1837f9741ff7bf58ce5d01e941253377b428f1
Sagar Bisht <itssagar11@gmail.com>
2022-10-24 12:41:26 +0530

    Fs and Path Module Example

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const http=require('http');
+const fs= require('fs');
+const path=require('path');
 const { hostname } = require('os');
 const host='localhost'
 const port='8080'
 
 const server= http.createServer((req,resp)=>{
-    console.log(req);
-    resp.statusCode=200;
+    console.log(req.url);
+   if(req.method=='GET'){
+    let fileUrl;
+    if(req.url=='/'){
+        resp.statusCode=200;
+        resp.setHeader('CONTENT_TYPE','text/html');
+        resp.end('<html><body><h1>Namaste, World!</h1></body></html>');
+    
+    }else{
+        fileUrl=req.url;
+        let filePath=path.resolve('./public'+fileUrl);
+        const fileExt= path.extname(filePath);
+        if(fileExt=='.html'){
+            fs.exists(filePath,(exist)=>{
+                console.log(filePath)
+                if(!exist){
+                    resp.statusCode=404;
+                    resp.setHeader('CONTENT_TYPE','text/html');
+                    resp.end('<html><body><small>file Not found!</small></body></html>');
+                
+                }else{
+                    resp.statusCode=200;
+                    resp.setHeader('CONTENT_TYPE','text/html');
+                    fs.createReadStream(filePath).pipe(resp);
+                
+                }
+            });
+        }else{
+            resp.statusCode=404;
+            resp.setHeader('CONTENT_TYPE','text/html');
+            resp.end('<html><body><small>File Not Supported!</small></body></html>');
+        
+        }
+    }
+   }else{
+    resp.statusCode=404;
     resp.setHeader('CONTENT_TYPE','text/html');
-    resp.end('<html><body><h1>Namaste, World!</h1></body></html>');
+    resp.end('<html><body><small>Request Method Not Supported!</small></body></html>');
 
+   }
 })
 server.listen(port,hostname,()=>{
     console.log("Server Started on Port 8080")
